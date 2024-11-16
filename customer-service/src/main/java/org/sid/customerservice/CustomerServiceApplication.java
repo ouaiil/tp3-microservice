dbed82b6dbebef2d485d569f61d79dcf5b1fce06
ouaiil <ouailkhali2@gmail.com>
2024-11-16 22:18:57 +0100

    SC

--- customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -1,10 +1,12 @@
 package org.sid.customerservice;
 
+import org.sid.customerservice.config.CustomerConfigParams;
 import org.sid.customerservice.entities.Customer;
 import org.sid.customerservice.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -12,6 +14,7 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import java.util.List;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
